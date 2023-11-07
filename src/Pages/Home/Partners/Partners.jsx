8db662f8c69c9d23a9af0c6958112aee6c6e49eb
anikdas400 <anikchandra400@gmail.com>
2023-11-07 12:56:35 +0600

    extra partners section complite

--- src/Pages/Home/Partners/Partners.jsx
+++ src/Pages/Home/Partners/Partners.jsx
@@ -1,10 +1,16 @@
-
+import Marquee from "react-fast-marquee";
 
 const Partners = () => {
     return (
         <div>
-            <h2 className="text-center text-4xl font-bold text-green-700">Partners</h2>
-            
+            <h2 className="text-center text-4xl font-bold mb-5 mt-7 text-green-700">Partners</h2>
+            <Marquee className="mb-7" pauseOnHover={true} speed={30}>
+               <img className="h-44 w-72" src="https://i.ibb.co/M9kNCsY/n.png" alt="" />
+               <img className="h-44 w-80" src="https://i.ibb.co/4YNgmh1/p.png" alt="" />
+               <img className="h-44 w-72" src="https://i.ibb.co/N9rNpYj/global.png" alt="" />
+               <img className="h-44 w-80" src="https://i.ibb.co/kD2MNWG/zendesk-logo-icon-167934.png" alt="" />
+            </Marquee>
+
         </div>
     );
 };
