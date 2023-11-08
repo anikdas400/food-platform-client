1d4ad32e422a79d8532fbb987830f0e2598385e0
anikdas400 <anikchandra400@gmail.com>
2023-11-08 17:32:30 +0600

    details page complite

--- src/Product/Details.jsx
+++ src/Product/Details.jsx
@@ -3,14 +3,14 @@ import { Link, useLoaderData } from "react-router-dom";
 
 const Details = () => {
     const food = useLoaderData()
-    const { _id, foodn, quantity, photo, date, description, location, imgUrl, dname }= food
+    const {foodn, quantity, photo, date, description, imgUrl, dname }= food
     return (
         <div className="card bg-base-100 shadow-xl">
-        <figure><img className="h-44 w-72 mt-3 rounded-lg ml-3" src={photo} alt="food" /></figure>
-        <div className="card-body">
+        <figure><img className="h-96 w-auto mt-3 rounded-lg ml-3" src={photo} alt="food" /></figure>
+        <div className="card-body ml-80">
             <h2 className="card-title">Food: <span className="text-purple-700">{foodn}</span></h2>
             <h3 className="text-lg font-semibold italic">{description}</h3>
-            <div className="flex gap-3">
+            <div className="flex gap-3 ">
                 <h2 className="font-semibold">quantity:<span className="text-red-700 font-bold">{quantity}</span></h2>
                 <p className="font-semibold">expired: <span className="text-red-500 font-bold">{date}</span></p>
 
@@ -23,7 +23,7 @@ const Details = () => {
                </div>
 
                 <div className=" justify-end">
-                <Link to='/'><button>Go Home</button></Link>
+                <Link to='/'><button className="btn btn-outline btn-secondary">Go Home</button></Link>
                 </div>
             </div>
             
